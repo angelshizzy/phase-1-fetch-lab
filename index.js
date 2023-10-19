486cbf9e4bb4b66e03641bf18dc77295f5ed89db
Angela Shirao <angela.shirao@student.moringaschool.com>
2023-10-19 06:35:20 +0300

    assignment

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
+const apiUrl= 'https://anapioficeandfire.com/api'
+
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch(`${apiUrl}/books`)
+  .then(res => res.json())
+  .then(data=> console.log(data))
 }
-
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
-}
+fetchBooks()
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
